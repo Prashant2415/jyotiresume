320d7bce5ea55beaf675981eaa9c24a3039c9bc0
“Prashant2415” <prashantmendhejbp@gmail.com>
2026-03-03 22:52:27 +0530

    update the contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,11 +7,17 @@ const Contact = () => {
     <section className='contact' id='contact'>
         <h2>{contactInfo.title}</h2>
         <div className="contact-container">
-            <p className='contact-description'>{contactInfo.description}</p>
+            {/* <p className='contact-description'>{contactInfo.description}</p>
             <div className="contact-card">
                 <p className='contact-email'>Email: <a href={`mailto:${contactInfo.email}`}>{contactInfo.email}</a></p>
             <p className='contact-location'>Location: {contactInfo.location}</p>
             <a href={`mailto:${contactInfo.email}`} className="contact-button">{contactInfo.cta}</a>
+            </div> */}
+            <p className='contact-description'>{contactInfo.description}</p>
+            <div className='contact-card'>
+                <p className='contact-email'>Email: <a href={`mailto:${contactInfo.email}`}>{contactInfo.email}</a></p>
+                <p className='contact-location'>Location: {contactInfo.location}</p>
+                {/* <a href={`mailto:${contactInfo.email}`} className="contact-button">{contactInfo.cta}</a> */}
             </div>
         </div>
     </section>
